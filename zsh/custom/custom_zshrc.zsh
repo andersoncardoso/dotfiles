dc445537e816088eb4f775226c3bef7baca122b5
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-07-24 01:40:48 -0300

    adding zsh customizations

---
+++ zsh/custom/custom_zshrc.zsh
@@ -0,0 +1,7 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+alias l='ls -F'
+alias ll='ls -lah'
