c7deff56e73357f97cc79082395894c79e1331fb
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-05-17 16:28:20 -0300

    fixing pyenv install script

--- pyenv/install_pyenv.sh
+++ pyenv/install_pyenv.sh
@@ -1,5 +1,4 @@
-cd
-git clone git@github.com:andersoncardoso/pyenv.git .pyenv
+git clone git@github.com:andersoncardoso/pyenv.git ~/.pyenv
 
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bash_profile
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.zshrc
@@ -7,6 +6,7 @@ echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 
-cp ./virtualenv-auto-activate.sh ~/.pyenv/bin/
+cp virtualenv-auto-activate.sh ~/.pyenv/bin/
+echo 'source ~/.pyenv/bin/virtualenv-auto-activate.sh' >> ~/.bash_profile
 echo 'source ~/.pyenv/bin/virtualenv-auto-activate.sh' >> ~/.zshrc
 
--- pyenv/virtualenv-auto-activate.sh
+++ pyenv/virtualenv-auto-activate.sh
@@ -1,50 +1,17 @@
 #!/bin/bash
 # virtualenv-auto-activate.sh
-# 
+#
 # Installation:
 #   Add this line to your .bashrc or .bash-profile:
 #
 #       source /path/to/virtualenv-auto-activate.sh
 #
-#   Go to your project folder, run "virtualenv .venv", so your project folder
-#   has a .venv folder at the top level, next to your version control directory.
-#   For example:
-#   .
-#   ├── .git
-#   │   ├── HEAD
-#   │   ├── config
-#   │   ├── description
-#   │   ├── hooks
-#   │   ├── info
-#   │   ├── objects
-#   │   └── refs
-#   └── .venv
-#       ├── bin
-#       ├── include
-#       └── lib
-#
-#   The virtualenv will be activated automatically when you enter the directory.
-
-# Check for virtualenvwrapper
-if type workon >/dev/null 2>&1; then
-  VENV_WRAPPER=true
-else
-  VENV_WRAPPER=false
-fi
 
 function _virtualenv_auto_activate() {
-    if [ -e ".venv" ]; then
-        # Check for symlink pointing to virtualenv
-        if [ -L ".venv" ]; then
-          _VENV_PATH=$(readlink .venv)
-          _VENV_WRAPPER_ACTIVATE=false
-        # Check for directory containing virtualenv
-        elif [ -d ".venv" ]; then
-          _VENV_PATH=$(pwd -P)/.venv
-          _VENV_WRAPPER_ACTIVATE=false
+    if [ -e ".python-env" ]; then
         # Check for file containing name of virtualenv
-        elif [ -f ".venv" -a $VENV_WRAPPER = "true" ]; then
-          _VENV_PATH=$WORKON_HOME/$(cat .venv)
+        if [ -f ".python-env" -a $VENV_WRAPPER = "true" ]; then
+          _VENV_PATH=$WORKON_HOME/$(cat .python-env)
           _VENV_WRAPPER_ACTIVATE=true
         else
           return
@@ -55,13 +22,6 @@ function _virtualenv_auto_activate() {
             if $_VENV_WRAPPER_ACTIVATE; then
               _VENV_NAME=$(basename $_VENV_PATH)
               workon $_VENV_NAME
-            else
-              _VENV_NAME=$(basename `pwd`)
-              VIRTUAL_ENV_DISABLE_PROMPT=1
-              source .venv/bin/activate
-              _OLD_VIRTUAL_PS1="$PS1"
-              PS1="($_VENV_NAME)$PS1"
-              export PS1
             fi
             echo Activated virtualenv \"$_VENV_NAME\".
         fi
