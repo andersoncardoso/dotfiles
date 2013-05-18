415acd00bb08f099a5b0fc44acdb18139a58fa25
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-05-17 23:48:25 -0300

    adding several improvements to zshrc to work better with mac + pyenv

--- pyenv/install_pyenv.sh
+++ pyenv/install_pyenv.sh
@@ -1,12 +1,2 @@
 git clone git@github.com:andersoncardoso/pyenv.git ~/.pyenv
-
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bash_profile
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.zshrc
-
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-
-cp virtualenv-auto-activate.sh ~/.pyenv/bin/
-echo 'source ~/.pyenv/bin/virtualenv-auto-activate.sh' >> ~/.bash_profile
-echo 'source ~/.pyenv/bin/virtualenv-auto-activate.sh' >> ~/.zshrc
-
+git clone git@github.com:andersoncardoso/pyenv-autoenv.git ~/.pyenv/plugins/pyenv-autoenv
