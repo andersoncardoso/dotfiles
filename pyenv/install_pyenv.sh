4dc9b1ce87a0917d6b4b58da3cd57dc4cd7d8548
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-05-20 15:24:50 -0300

    fixing install for pyenv

--- pyenv/install_pyenv.sh
+++ pyenv/install_pyenv.sh
@@ -1,2 +1,18 @@
 git clone git@github.com:andersoncardoso/pyenv.git ~/.pyenv
 git clone git@github.com:andersoncardoso/pyenv-autoenv.git ~/.pyenv/plugins/pyenv-autoenv
+
+SHELL_NAME=`basename $( ps h p $$ | awk '{ print $NF }' )`
+if [ "$SHELL_NAME" = "zsh" ];then
+    FNAME="$HOME/.zshrc"
+else
+    FNAME="$HOME/.bashrc"
+fi
+
+echo "print to $FNAME"
+echo "" >> $FNAME
+echo "# settings up PYENV" >> $FNAME
+echo "if [[ -d \$HOME/.pyenv ]];then" >> $FNAME
+echo "    export PATH=\"\$HOME/.pyenv/bin:\$PATH\"" >> $FNAME
+echo "    eval \"\$(pyenv init -)\"" >> $FNAME
+echo "    source ~/.pyenv/plugins/pyenv-autoenv/bin/pyenv-autoenv" >> $FNAME
+echo "fi" >> $FNAME
