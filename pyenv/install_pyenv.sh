c94234a64f33d8536609d91a31749134f5e9e7f8
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-05-21 17:05:17 -0300

    fixing pyenv_install

--- pyenv/install_pyenv.sh
+++ pyenv/install_pyenv.sh
@@ -1,18 +1,3 @@
 git clone git://github.com/andersoncardoso/pyenv.git ~/.pyenv
 git clone git://github.com/andersoncardoso/pyenv-autoenv.git ~/.pyenv/plugins/pyenv-autoenv
 
-SHELL_NAME=`basename $( ps h p $$ | awk '{ print $NF }' )`
-if [ "$SHELL_NAME" = "zsh" ];then
-    FNAME="$HOME/.zshrc"
-else
-    FNAME="$HOME/.bashrc"
-fi
-
-echo "Adding settings to $FNAME"
-echo "" >> $FNAME
-echo "# settings up PYENV" >> $FNAME
-echo "if [[ -d \$HOME/.pyenv ]];then" >> $FNAME
-echo "    export PATH=\"\$HOME/.pyenv/bin:\$PATH\"" >> $FNAME
-echo "    eval \"\$(pyenv init -)\"" >> $FNAME
-echo "    source ~/.pyenv/plugins/pyenv-autoenv/bin/pyenv-autoenv" >> $FNAME
-echo "fi" >> $FNAME
---
+++ pyenv/install_pyenv_generic.sh
@@ -0,0 +1,24 @@
+git clone git://github.com/andersoncardoso/pyenv.git ~/.pyenv
+git clone git://github.com/andersoncardoso/pyenv-autoenv.git ~/.pyenv/plugins/pyenv-autoenv
+
+SHELL_NAME=`basename $( ps h p $$ | awk '{ print $NF }' )`
+if [ "$SHELL_NAME" = "zsh" ];then
+    FNAME="$HOME/.zshrc"
+else
+    FNAME="$HOME/.bashrc"
+fi
+
+echo >> $FNAME << EOL
+if [[ -d $HOME/.pyenv ]]; then
+    export PATH="$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH"
+    eval "$(pyenv init -)"
+    pyenv rehash 2>/dev/null
+    source ~/.pyenv/plugins/pyenv-autoenv/bin/pyenv-autoenv
+fi
+
+# virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+source ~/.pyenv/versions/$(pyenv global)/bin/virtualenvwrapper.sh
+EOL
+
+source "$FNAME"
