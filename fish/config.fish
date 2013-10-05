9f9fa8f3639de2a7e0e4c3c1c5c77e321ec7b2bc
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-10-05 00:37:14 -0300

    add some fixes to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,7 @@ set -U EDITOR vim
 set -x CODE_PATH ~/code
 
 set -x PATH $PATH $HOME/bin $HOME/.rvm/bin $CODE_PATH/elixir/bin $PATH
+
+# virtualenvwrapper
+. $CODE_PATH/virtualfish/virtual.fish
+. $CODE_PATH/virtualfish/global_requirements.fish
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,15 +1,15 @@
 set fish_git_dirty_color purple
 set fish_git_not_dirty_color green
-set fish_git_normal_color cyan
+set fish_title_color cyan
 
 function parse_git_branch
   set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
   set -l git_diff (git diff)
 
   if test -n "$git_diff"
-    echo (set_color $fish_git_dirty_color)$branch(set_color $fish_git_normal_color)
+    echo (set_color $fish_git_dirty_color)$branch(set_color $fish_title_color)
   else
-    echo (set_color $fish_git_not_dirty_color)$branch(set_color $fish_git_normal_color)
+    echo (set_color $fish_git_not_dirty_color)$branch(set_color $fish_title_color)
   end
 end
 
@@ -17,11 +17,14 @@ function fish_right_prompt
   set_color blue
   echo (prompt_pwd)
 
-  set_color $fish_git_normal_color
+  if set -q VIRTUAL_ENV
+      echo -n -s (set_color $fish_title_color) " venv(" (set_color purple) (basename "$VIRTUAL_ENV") (set_color $fish_title_color) ")"
+  end
 
   if test -d .git
+    set_color $fish_title_color
     echo " git:("(parse_git_branch)")"
+    set_color normal
   end
-  set_color normal
 
 end
